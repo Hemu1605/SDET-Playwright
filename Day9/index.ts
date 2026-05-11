e07bfa96ef2810578af32f5f6297236efef3f2c6
Hemraj0808 <hemrajgujjar0808@gmail.com>
2026-05-11 12:39:24 +0530

    ts

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,7 +1,11 @@
-function greet(name: string): string {
-    return `Hello, ${name}!`;
+let name: string = "Aaryan";
+let result: number | string;
+result = 45;
+result = "success";
+console.log(name, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
 }
-
-const user = "Hemraj";
-
-console.log(greet(user));
+ 
+console.log(add(5, 10));
