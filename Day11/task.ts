7e9bc840a2c29d54b26ee24fb636d048e8493639
Hemraj0808 <hemrajgujjar0808@gmail.com>
2026-05-13 14:25:46 +0530

    task

--- Day11/task.ts
+++ Day11/task.ts
@@ -67,62 +67,62 @@
 // Question 3:  Exhaustiveness Checking (The never Type)
 ////////////////////////////////////////////////////////////////////////
 
-type TaskStatus = "Open" | "InProgress" | "Closed";
+// type TaskStatus = "Open" | "InProgress" | "Closed";
 
-function handleTask(status: TaskStatus): string {
-  switch (status) {
-    case "Open":
-      return "Task is open";
+// function handleTask(status: TaskStatus): string {
+//   switch (status) {
+//     case "Open":
+//       return "Task is open";
 
-    case "InProgress":
-      return "Task is in progress";
+//     case "InProgress":
+//       return "Task is in progress";
 
-    case "Closed":
-      return "Task is closed";
+//     case "Closed":
+//       return "Task is closed";
 
-    default:
-      const exhaustiveCheck: never = status;
-      return exhaustiveCheck;
-  }
-}
+//     default:
+//       const exhaustiveCheck: never = status;
+//       return exhaustiveCheck;
+//   }
+// }
 
-console.log(handleTask("Open"));
-console.log(handleTask("InProgress"));
-console.log(handleTask("Closed"));
+// console.log(handleTask("Open"));
+// console.log(handleTask("InProgress"));
+// console.log(handleTask("Closed"));
 
 ////////////////////////////////////////////////////////////////////////
 // Question 4:  Recursive Navigation Type
 ////////////////////////////////////////////////////////////////////////
 
 
-type FolderNode = {
-  name: string;
-  files?: string[];
-  subFolders?: FolderNode[];
-};
+// type FolderNode = {
+//   name: string;
+//   files?: string[];
+//   subFolders?: FolderNode[];
+// };
 
-const myFolder: FolderNode = {
-  name: "Root",
-  files: ["index.html", "style.css"],
-  subFolders: [
-    {
-      name: "Images",
-      files: ["logo.png", "banner.jpg"],
-    },
-    {
-      name: "Scripts",
-      files: ["app.js"],
-      subFolders: [
-        {
-          name: "Utils",
-          files: ["helper.js"],
-        },
-      ],
-    },
-  ],
-};
+// const myFolder: FolderNode = {
+//   name: "Root",
+//   files: ["index.html", "style.css"],
+//   subFolders: [
+//     {
+//       name: "Images",
+//       files: ["logo.png", "banner.jpg"],
+//     },
+//     {
+//       name: "Scripts",
+//       files: ["app.js"],
+//       subFolders: [
+//         {
+//           name: "Utils",
+//           files: ["helper.js"],
+//         },
+//       ],
+//     },
+//   ],
+// };
 
-console.log(myFolder);
+// console.log(myFolder);
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -130,14 +130,131 @@ console.log(myFolder);
 ////////////////////////////////////////////////////////////////////////
 
 
-type Unit = "px" | "rem" | "vh";
+// type Unit = "px" | "rem" | "vh";
 
-type MarginValue = `${number}${Unit}`;
+// type MarginValue = `${number}${Unit}`;
 
-let margin1: MarginValue = "10px";
-let margin2: MarginValue = "2rem";
-let margin3: MarginValue = "50vh";
+// let margin1: MarginValue = "10px";
+// let margin2: MarginValue = "2rem";
+// let margin3: MarginValue = "50vh";
 
-console.log(margin1);
-console.log(margin2);
-console.log(margin3);
+// console.log(margin1);
+// console.log(margin2);
+// console.log(margin3);
+
+
+////////////////////////////////////////////////////////////////////////
+// Question 6:  Conditional Types & the infer Keyword
+////////////////////////////////////////////////////////////////////////
+
+
+// type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
+
+// type Result1 = UnwrapPromise<Promise<string>>;
+// type Result2 = UnwrapPromise<Promise<number>>;
+// type Result3 = UnwrapPromise<boolean>;
+
+// let value1: Result1 = "Hello";
+// let value2: Result2 = 100;
+// let value3: Result3 = true;
+
+// console.log(value1);
+// console.log(value2);
+// console.log(value3);
+
+////////////////////////////////////////////////////////////////////////
+// Question 7:  The Union Manipulation Puzzle
+////////////////////////////////////////////////////////////////////////
+
+// type AllEvents = "click" | "dbclick" | "submit" | "reset" | "keypress";
+
+// type MouseEvents = Extract<AllEvents, "click" | "dbclick">;
+
+// type NonFormEvents = Exclude<AllEvents, "submit" | "reset">;
+
+// let event1: MouseEvents = "click";
+// let event2: MouseEvents = "dbclick";
+
+// let event3: NonFormEvents = "click";
+// let event4: NonFormEvents = "dbclick";
+// let event5: NonFormEvents = "keypress";
+
+// console.log(event1);
+// console.log(event2);
+// console.log(event3);
+// console.log(event4);
+// console.log(event5);
+
+
+////////////////////////////////////////////////////////////////////////
+// Question 8:  Async Higher-Order Function (HOF)
+////////////////////////////////////////////////////////////////////////
+
+// function safeExecute<T>(asyncFn: () => Promise<T>): () => Promise<T | null> {
+//   return async function () {
+//     try {
+//       const result = await asyncFn();
+//       return result;
+//     } catch (error) {
+//       console.error("Error occurred:", error);
+//       return null;
+//     }
+//   };
+// }
+
+// // Example async function
+// async function fetchUser(): Promise<string> {
+//   return "Hemraj";
+// }
+
+// // Wrap the function
+// const safeFetchUser = safeExecute(fetchUser);
+
+// // Run it
+// safeFetchUser().then((result) => {
+//   console.log(result);
+// });
+
+
+////////////////////////////////////////////////////////////////////////
+// Question 9:  Index Signatures for Dynamic Metadata
+////////////////////////////////////////////////////////////////////////
+
+
+// interface UserMetadata {
+//   createdAt: Date;
+//   [key: string]: string | number | boolean | Date;
+// }
+
+// const user: UserMetadata = {
+//   createdAt: new Date(),
+//   username: "Hemraj",
+//   age: 22,
+//   isActive: true,
+// };
+
+// console.log(user);
+
+
+////////////////////////////////////////////////////////////////////////
+// Question 10:  Mapped Types with Key Remapping
+////////////////////////////////////////////////////////////////////////
+
+
+interface Car {
+  make: string;
+  model: string;
+}
+
+type ApiResponse<T> = {
+  [K in keyof T as `DATA_${Uppercase<string & K>}`]: T[K];
+};
+
+type CarApiResponse = ApiResponse<Car>;
+
+const carData: CarApiResponse = {
+  DATA_MAKE: "Toyota",
+  DATA_MODEL: "Fortuner",
+};
+
+console.log(carData);
